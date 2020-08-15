80abaef453d4b50eb53896d4bbbd40acf3d796d6
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-15 16:53:03 -0400

    Create noSpace.js

---
+++ noSpace.js
@@ -0,0 +1,11 @@
+//Write a function that removes the spaces from the values and returns an array showing the space decreasing - codewars
+
+function spacey(array){
+  let space = ""
+  return array.map( (x) => space += x)
+  // return in this manner to show the space decreasing.
+  //returning variable will show final result
+}
+
+//['kevin', 'has','no','space'] returns [ 'kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace']);
+//['this','cheese','has','no','holes'] rreturns ['this','thischeese','thischeesehas','thischeesehasno','thischeesehasnoholes']);
