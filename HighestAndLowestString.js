a1c1497e670e14f47712bc0fa09016ce2513a553
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-15 13:35:14 -0400

    Create HighestAndLowestString.js

---
+++ HighestAndLowestString.js
@@ -0,0 +1,22 @@
+//return largest and smallest numbers found in a string - codewars
+
+function highAndLow(numbers){
+  let arr = numbers.split(" ")
+  //split numbers in string, makes them into array in order to test
+  let large = arr[0]
+  let small = arr[0]
+  for (let i in arr){
+    if (parseInt(large) < parseInt(arr[i])) {
+      large = arr[i]
+      //this tests for largest possible number
+    } if (parseInt(small) > parseInt(arr[i])){
+      small = arr[i]
+      // this tests for smallest possible number
+    }
+  }return large + " " + small
+  //concatenate numbers
+}
+
+//highAndLow("1 2 3 4 5");  // return "5 1"
+//highAndLow("1 2 -3 4 5"); // return "5 -3"
+//highAndLow("1 9 3 4 -5"); // return "9 -5"
