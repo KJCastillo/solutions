a4debb985b04fb2729a6cc0da17cb1f8e27e839c
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-15 13:05:21 -0400

    Create getMiddleCharacter.js

---
+++ getMiddleCharacter.js
@@ -0,0 +1,19 @@
+//get middle character of a string - codewars
+
+function getMiddle(s)
+{
+  var middle = Math.floor(s.length/2);
+    //Math.floor to get round number when checking length of string
+    
+    if (s.length % 2 == 0)
+        return s[middle-1] + s[middle]
+        //scenario if string equals even number
+    else
+        return s[middle]
+        //scenario if string equals even number
+}
+
+//Test.assertEquals(getMiddle("test"),"es");
+//Test.assertEquals(getMiddle("testing"),"t");
+//Test.assertEquals(getMiddle("middle"),"dd");
+//Test.assertEquals(getMiddle("A"),"A");
