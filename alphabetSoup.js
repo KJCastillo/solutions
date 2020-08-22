d366fd750a69ff7332d37d990819319c1d7e966f
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-22 11:16:18 -0400

    Create alphabetSoup.js

---
+++ alphabetSoup.js
@@ -0,0 +1,15 @@
+//take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo) 
+//assume numbers and punctuation symbols will not be included in the string - coderbyte
+
+function AlphabetSoup(str) { 
+
+  let a = str.split("")
+  //split string individually
+  let b = a.sort()
+  //sort method will make them in alphabetical order
+  let c = b.join("")
+  //join string back together
+  return c
+}
+
+//console.log(AlphabetSoup(coderbyte)) = bcdeeorty
