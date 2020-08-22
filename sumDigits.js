d89bc399d57ec24d3b0204a1ac5dfa62823676c3
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-22 11:01:47 -0400

    Update sumDigits.js

--- sumDigits.js
+++ sumDigits.js
@@ -2,7 +2,7 @@
 //for example:5646 => 5+6+4+6 => 21 - 4Geeks
 
 function sumDigits(num) {
- let nums = (num.toString())).split('').map((i) => { return Number(i); })
+ let nums = (num.toString()).split('').map((i) => { return Number(i); })
   //make into string in order string, split to make them solo, then map through them to return them into integers
   return nums.reduce((a,b) => a + b, 0)
   //reduce method to find sum
