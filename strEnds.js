fd3fa89509f8592d8631f6f7dad6818889ede666
Kevin Castillo <kevincastillo318@gmail.com>
2020-08-24 10:34:43 -0400

    Create strEnds.js

---
+++ strEnds.js
@@ -0,0 +1,10 @@
+//complete the solution so that it returns true if the first argument(string) 
+//passed in ends with the 2nd argument (also a string) - codewars
+
+function solution(str, ending){
+ return str.endsWith(ending)
+ //endsWith() method checks for paramater at end of string
+}
+
+//solution('abc', 'bc') = returns true
+//solution('abc', 'd') = returns false
