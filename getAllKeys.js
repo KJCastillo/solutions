07a1977485c3a5edb6eb4bca42f15b9f541ede27
Kevin Castillo <kevincastillo318@gmail.com>
2020-09-02 18:01:57 -0400

    Create getAllKeys.js

---
+++ getAllKeys.js
@@ -0,0 +1,23 @@
+//Write a function called "getAllKeys" which returns an array of all the input object's keys.
+//Example input: 
+//{
+//  name : 'Sam',
+//  age : 25,
+//  hasPets : true
+//}
+//Example output: ['name', 'age', 'hasPets']
+
+function getAllKeys(obj) {
+  return Object.keys(obj)
+  //Object.keys method returns all the object's keys in an array
+}
+
+//let output = 
+//{
+//  name : 'Sam',
+//  age : 25,
+//  hasPets : true
+//}
+
+//console.log(getAllKeys(output))
+//output: ['name', 'age', 'hasPets']
