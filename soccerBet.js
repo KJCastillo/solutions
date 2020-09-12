9151f5ca00fcaed17e1efb6b5cae23ae5d9a3d49
Kevin Castillo <kevincastillo318@gmail.com>
2020-09-12 10:52:26 -0400

    Create soccerBet.js

---
+++ soccerBet.js
@@ -0,0 +1,9 @@
+//For each line on your ticket, check whether or not the result matches. If it does, you win the amount specified on the ticket for that match. Like this:
+
+//Away:£20
+//Home:£10
+//Draw:£5
+//[1-0, 2-1, 0-1]
+//Results in Winnings = £10 (first bet lost, second bet won, third bet lost);
+
+//Return your total winning as a string with the £ sign like this: '£100'
