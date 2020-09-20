d2f3691d47c4b3dbf4c3127dd044091f1f3c9383
Kevin Castillo <kevincastillo318@gmail.com>
2020-09-20 15:52:53 -0400

    Update uppercaseFirstLetter.js

--- uppercaseFirstLetter.js
+++ uppercaseFirstLetter.js
@@ -1,7 +1,8 @@
 //take a string and return it with every first letter capitalized - codewars
 
 String.prototype.toJadenCase = function () {
-  return this.split(" ").map( x => x[0].toUpperCase()+ x.substring(1, this.length -1 )).join(' ')
+  return this.split(" ").map( x => x[0].toUpperCase() + x.substring(1, this.length -1 )).join(' ')
+  //split the string into words, upper case the first letter(postion 0), concat the sub string and join words
 };
 
 //var str = "How can mirrors be real if our eyes aren't real";
