57d185ed8b01d07ad3e68417d150385694ff011e
Kevin Castillo <kevincastillo318@gmail.com>
2022-01-13 14:59:09 -0800

    Create returnNegative.js

---
+++ returnNegative.js
@@ -0,0 +1,12 @@
+// In this simple assignment you are given a number and have to make it negative.
+// The number can be negative already, in which case no change is required.
+
+function makeNegative(num) {
+  if (num < 0){
+    return num
+    //if number already negative, return it
+  } else {
+    return parseFloat('-' + num)
+    //if number postive, add negative sign in front and parseFloat to turn string into number
+  }
+}
